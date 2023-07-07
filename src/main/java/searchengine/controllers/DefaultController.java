0617c1b8c1d128d504618f5797aab320298de6ca
Phozar <phozar@ya.ru>
2023-07-07 13:00:36 +0300

    Code cleaning

--- src/main/java/searchengine/controllers/DefaultController.java
+++ src/main/java/searchengine/controllers/DefaultController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DefaultController {
 
-    /**
-     * Метод формирует страницу из HTML-файла index.html,
-     * который находится в папке resources/templates.
-     * Это делает библиотека Thymeleaf.
-     */
     @RequestMapping("/")
     public String index() {
         return "index";
